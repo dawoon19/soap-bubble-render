99bcb05604169c22115f1ab89278566f2e1f0d76
Daniel Won <danielwon23@berkeley.edu>
2022-04-01 12:54:17 -0700

    huh

--- src/pathtracer/camera_lens.cpp
+++ src/pathtracer/camera_lens.cpp
@@ -28,42 +28,42 @@ Ray Camera::generate_ray_for_thin_lens(double x, double y, double rndR, double r
     // Red segment
     float hFov_rad = hFov * (PI / 180);
     float vFov_rad = vFov * (PI / 180);
-      
+
     float max_x = tan(0.5 * hFov_rad);
     float max_y = tan(0.5 * vFov_rad);
-      
+
     float offset_x_img = x - 0.5;
     float offset_y_img = y - 0.5;
-      
+
     float scale_x = max_x / 0.5;
     float scale_y = max_y / 0.5;
 
     float x_cam = scale_x * offset_x_img;
     float y_cam = scale_y * offset_y_img;
-      
+
     Vector3D d = Vector3D(x_cam, y_cam, -1);
     d.normalize();
-    
+
     Vector3D pLens = Vector3D(lensRadius * sqrt(rndR) * cos(rndTheta),
                             lensRadius * sqrt(rndR) * sin(rndTheta),0);
-    
-    
-    double t = dot(Vector3D(0, 0, -focalDistance) - pos, Vector3D(0, 0, 1)) / dot(d, Vector3D(0, 0, 1));
+
+
+    double t = dot(Vector3D(0, 0, -focalDistance), Vector3D(0, 0, 1)) / dot(d, Vector3D(0, 0, 1));
     Vector3D pFocus = t * d;
-    
+
     Vector3D blue_sgmt = pFocus - pLens;
     blue_sgmt.normalize();
-    
+
     Ray finalRay = Ray(pLens, blue_sgmt);
     finalRay.o = c2w * finalRay.o;
     finalRay.o += pos;
-    
+
     finalRay.d = c2w * finalRay.d;
     finalRay.d.normalize();
-    
+
     finalRay.min_t = nClip;
     finalRay.max_t = fClip;
-    
+
     return finalRay;
 }
 
